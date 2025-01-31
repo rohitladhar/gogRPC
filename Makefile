8dc2b32428c4e09476bcfd5b864d78bfcea2aa2e
rohit.ladhar <rohit.ladhar10@gmail.com>
2025-01-31 14:10:46 +0000

    proto file

---
+++ Makefile
@@ -0,0 +1,3 @@
+protoc --go_out=./coffeeshop_proto --go_opt=paths=source_relative \
+    --go-grpc_out=./coffeeshop_proto --go-grpc_opt=paths=source_relative \
+    coffee_shop.proto
---
+++ server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+
+	pb "github.com/rohitladhar/gogRPC/coffeeshop_proto"
+)
+
+type server struct {
+	pb.UnimplementedCoffeeShopServer
+}
+
+func (s *server) GetMenu(menuRequest *pb.MenuRequest, srv pb.CoffeeShop_GetMenuServer) error {
+	items := []*pb.Item {
+		&pb.Item{
+			Id:   "1",
+			Name: "Black Coffee",
+		},
+		&pb.Item{
+			Id:"2",
+			Name:"Americano",
+		},
+		&pb.Item{
+			Id:"3",
+			Name:"Vanilla Latte",
+		},
+	}
+
+	for i, _ := range items {
+		srv.Send(&pb.Menu{
+			Items: items[0: i+1],
+		})
+	}
+	return nil
+}
+func (s *server) PlaceOrder(context context.Context, order *pb.Order) (*pb.Receipt, error) {
+	return &pb.Receipt{
+		Id:"ABC123",
+	}, nil
+}
+func (s *server) GetOrderStatus(context context.Context,receipt *pb.Receipt) (*pb.OrderStatus, error) {
+	return &pb.OrderStatus{
+		OrderId: receipt.Id,
+		Status: "In Progress",
+	},nil
+}
